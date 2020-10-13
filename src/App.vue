7b7d8a4dc23c843aac574ff954cafdb52f156a80
Shashwat Chandra <chandra.shashwat@gmail.com>
2020-10-13 21:40:20 +0530

    adf

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="app" style="margin-top: 0; padding-top: 60px;">
+    <div class="container" v-if="fileList">
+      <div class="columns">
+        <div class="column">
+          <div class="level is-centered">
+            <div class="level-item">
+              <p class="title is-3">
+                Happy Birthday!
+              </p>
+            </div>
+            <div class="level-item">
+              <template v-if="playingFile.name">
+                Playing {{ playingFile.name }}
+              </template>
+            </div>
+            <div class="level-item">
+              <figure class="image is-128x128">
+                <img class="is-rounded" src="pic.jpg">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-12 has-text-centered">
+          <audio
+            ref="player"
+            controls
+            :src="playingFile.src">
+          </audio>
+        </div>
+      </div>
+      <div class="columns is-multiline">
+        <div class="column is-8 is-offset-2">
+          <input type="text" v-model="textFilter" class="input" placeholder="Search">
+        </div>
+        <div class="column is-4" v-for="item in filteredList(fileList.files)">
+          <div class="card">
+            <div class="card-header">
+              <p class="card-header-title">
+                {{ filteredName(item) }}
+              </p>
+            </div>
+            <div class="card-content">
+              {{ item }}
+            </div>
+            <footer class="card-footer">
+              <a href="#" class="card-footer-item" @click="playSong(item)">Play</a>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      fileList: null,
+      textFilter: "",
+      playingFile: {
+        src: null,
+        name: null
+      }
+    };
+  },
+  methods: {
+    filteredList (list) {
+      return list.filter((v) => v.name.toLowerCase().includes(this.textFilter.toLowerCase()))
+                 .sort((a,b) => (a.name < b.name ? -1 : 1));
+    },
+    filteredName (item) {
+      return item.name.slice(0, -4);
+    },
+    playSong (item) {
+      this.playingFile = {
+        src: "https://drive.google.com/uc?id=" + item.id,
+        name: this.filteredName(item)
+      }
+      window.setTimeout(() => this.$refs.player.play(), 0);
+    }
+  },
+  created () {
+    fetch('filelist.json')
+      .then(resp => resp.json())
+      .then((data) => {
+        this.fileList = data;
+      });
   }
 }
 </script>
@@ -24,5 +102,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  background-color: aliceblue;
 }
 </style>
